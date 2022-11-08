eb9c48db7747d77b6b69a43b50fa017bb99b1150
ZZZaratustra <varlamcheg@inbox.ru>
2022-11-08 15:08:39 +0300

    union streams in server commit

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 import express from 'express'
 import fs from 'fs'
 const app = express()
-app.get('/', (req,res)=> res.send('вроде робит'))
+
+app.get('/', (req,res)=> {
+
+    const readFile = (fileName) => ()=> {
+
+        const readStream = fs.createReadStream(fileName)
+        const reads = readStream.on('readable', () => {
+
+            const data = readStream.read()
+            console.log(data)
+            console.log(data?.toString())
+            return data?.toString()
+
+        })
+        console.log("reads:",reads())
+        return reads.toString()
+    }
+
+    console.log("readFile('input.txt'):",readFile('input.txt'))
+    // res.write('вроде робит', readFile('input.txt'))
+})
+
 app.listen(3000)
 
-const readFile = (fileName) => ()=> {
-    const readStream = fs.createReadStream(fileName)
-    const reads = readStream.on('readable', () => {
-    const data = readStream.read()
-    console.log(data)
-    return data?.toString()
-    })
-    console.log("reads:",reads())
-}
-console.log("readFile('input.txt'):",readFile('input.txt'))
+// app.get('/', (req,res) => {
+    //    const readStream = createReadStream(firstFileName)
+    //    readStream.on('data', (data) => {
+    //        res.write(data)
+    //    })
+    //    readStream.on('end', (data)=>{
+    //        res.status(200).send()
+    //    })
+    //})
+    //app.listen(3000)
